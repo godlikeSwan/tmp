ec4a706f1bbaf50e324857f8e3c83ee74d7c1087
Lebedev Ilya <godlikeSwan@gmail.com>
2020-11-17 15:13:53 +0000

    cleared

--- algorithms/recursion.js
+++
@@ -1,6 +0,0 @@
-function countdown (n) {
-  console.log(n)
-  if (n <= 0) return
-  countdown(n - 1)
-}
-countdown(15)
--- algorithms/sum.js
+++
@@ -1,5 +0,0 @@
-function sum (arr) {
-  if (arr.length === 0) return 0
-  return arr[0] + sum(arr.slice(1))
-}
-console.log(sum([1, 5, 3, 2]))
